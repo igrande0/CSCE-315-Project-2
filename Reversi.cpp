d1c64542111e476e51f6bcb5f203ac1f64801016
Jake Tyler Mcknight <jakemc321@tamu.edu>
2013-10-07 17:06:51 -0500

    basic structure

--- Reversi.cpp
+++ Reversi.cpp
@@ -13,6 +13,7 @@ void Reversi::display_board(){
 			else
 				cout<<"_|";
 		}
+		cout<<"a b c d e f g h"<<endl;
 		cout<<endl;
 	}
 			
@@ -21,7 +22,7 @@ void Reversi::display_board(){
 void Reversi::clear_board(){
 	for(unsigned int i=0; i<8; ++)
 		for(unsigned int j=0; j<8 j++)
-			board[i][j] = 'c';
+			board[i][j] = 'o';
 	board[3][3] = 'w';
 	board[3][4] = 'b';
 	board[4][3] = 'b';
@@ -47,14 +48,27 @@ vector<int, char> Reversi::get_avalible_moves(char side){
 		opponent = 'b';
 	else if(side == 'b')
 		opponent = 'w';
-	
+	vector<int, char> temp_vec;
+	//needs to be finished
+	vector<int, char> open_spaces = get_open_spaces();				//need to check every direction for every space
+	/*for(unsigned int i=0; i<open_spaces.size(); i++){
+		//check if board[i][j] is a possible move
+		int temp_x = i;
+		int temp_y = j;
+		int x_step = 0;
+		int y_step = 0;
+		while(temp_x < 8 && temp_y < 8 && board[temp_x][temp_y] != 'o'){ //check above
+
+		}
+	}*/
+	return temp_vec;
 }
 
 vector<int, char> Reversi::get_open_spaces(){
 	vector<int, char> open_spaces;		//creates temp vector to hold open spaces
 	for(unsigned int i=0; i<8; ++)
 		for(unsigned int j=0; j<8 j++)
-			if(board[i][j] == 'c')									//if the space is clear then push the integer and char of space
+			if(board[i][j] == 'o')									//if the space is clear then push the integer and char of space
 				open_spaces.push_back((i+1),get_letter_of_number(j+1));		
 	return open_spaces;												//return temp vector containing all open spaces
 }
@@ -84,6 +98,38 @@ char Reversi::get_letter_of_number(int number){
 			break;
 		case 8:
 			return 'h';
+			break;
+		default:
+			return 'x';
+			break;
+	}
+}
+
+int Reversi::get_number_of_letter(char c){
+	switch (c){
+		case 'a':
+			return 1;
+			break;
+		case 'b':
+			return 2;
+			break;
+		case 'c':
+			return 3;
+			break;
+		case 'd':
+			return 4;
+			break;
+		case 'e':
+			return 5;
+			break;
+		case 'f':
+			return 6;
+			break;
+		case 'g':
+			return 7;
+			break;
+		case 'h':
+			return 8;
 			break;
 		default:
 			return -1;
--- Reversi.h
+++ Reversi.h
@@ -1,12 +1,49 @@
 class Reversi{
-	private:
-		vector<vector<char>> board;
-		int white_score;
-		int black_score;
 	public:
-		void display_board();
 		void clear_board();
 		void update_score();
-		vector<int,char> get_open_spaces();
+		vector<int, char> get_open_spaces();
+		
+		/* POSSIBLE FUNCTIONS */
+		
+		/*	play_game();
+			LOOP
+			-----SEND STATE THROUGH SOCKET-----
+			send board string
+			send moves
+			send score
+			-----GET USER'S MOVE-----
+			send state
+			read user's move 
+			make_move(user's move)
+			is_game_over()
+			-----MAKE RANDOM MOVE-----
+			send state
+			make_random_move()
+			is_game_over()
+			
+			END GAME
+		*/
+		void play_game(int file_descriptor);
+		
+		void make_random_move();
+		void make_move(string move);
+		
+		bool is_valid_move(string move);
+		bool is_game_over();
+		
+		string get_state();
+	private:
+		// STATE VARIABLES
+		vector<vector<char>> board;
+		vector<int, char> available_moves;
+		stack<int, char> previous_moves;
+		void display_state();
+		int white_score;
+		int black_score;
+		
+		// PRIVATE UTILITY FUNCTIONS
 		char get_number_of_letter(int number);
+		int get_letter_of_number(char c);
+		vector<int, char> get_available_moves();
 }
