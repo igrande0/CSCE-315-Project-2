50b97466a82642ba21fa19a8c1648c081afa3213
Nicholas Joseph Giunta <nickjgiunta@gmail.com>
2013-10-21 16:32:43 -0500

    Added AIMain.cpp

---
+++ AI/AIMain.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <unistd.h>
+
+#include "AI.h"
+#include "Reversi.h"
+
+using namespace std;
+
+int main(){
+	cout << "Entered main\n";
+	AI ai;
+	cout << "Initialized AI\n";
+	Reversi game;
+	cout << "Easy move string: " << ai.get_move(game, AI::EASY) << "\nMedium move string: " << ai.get_move(game, AI::MEDIUM) << "\nHard move string: " << ai.get_move(game, AI::HARD) << '\n';
+	cout << "Beginning game play!\n";
+	while(!game.is_game_over()) {
+		cout << ";----------MAKE HARD MOVE----------\n";
+		game.make_move(ai.get_move(game,AI::HARD));
+		cout << "MOVE:" << game.get_previous_move() << '\n';
+		cout << game.get_state_string();
+		sleep(2);
+	}
+	return 0;
+}
